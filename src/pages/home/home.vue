0df5e295ffdaa25f397984206f4dfb60f2b05b4f
yangyin <806338925@qq.com>
2017-12-05 17:49:20 +0800

    音乐播放页

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,12 +1,24 @@
 <template>
-  <keep-alive>
-    <router-view/>
-  </keep-alive>
+  <div>
+    <Video></Video>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+  </div>
+
 </template>
 
 <script>
+import Video from '../../components/video/index';
 export default {
+  data() {
+    return {
 
+    }
+  },
+  components: {
+    Video
+  }
 }
 </script>
 
