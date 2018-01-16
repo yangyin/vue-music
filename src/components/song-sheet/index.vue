4fe752d5040684dda20ce9c1ad1c31ddc80ba9f3
Administrator <182316962@qq.com>
2018-01-16 23:44:30 +0800

    样式

--- src/components/song-sheet/index.vue
+++ src/components/song-sheet/index.vue
@@ -1,17 +1,39 @@
 <template>
   <div>
-      歌单列表
+    <header>
+        <TopBar>
+            <div class="title">
+                <div slot="left">
+                    <router-link to="/">
+                        <i class="iconfont icon-iconfontzuojiantou"></i>
+                    </router-link>
+                </div>
+                
+                <div slot="right">
+                    <router-link to="/" >
+                        <i class="iconfont icon-sousuo"></i>
+                    </router-link>
+                    <i class="iconfont icon-fengexianICONCopy"></i> 
+                </div>
+            </div>
+        </TopBar>              
+    </header>    
   </div>
 </template>
 
 <script>
 import http from '../../http';
 import api from '../../http/api';
+
+import TopBar from '../topBar.vue';
 export default {
     data() {
         return {
 
         }
+    },
+    components: {
+        TopBar
     },
     created () {
         let id = this.$route.query.id;
@@ -26,6 +48,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    a {
+        text-decoration: none;
+        i {
+            font-size: 2.5rem;
+        }
+    }
+}
 </style>
 
