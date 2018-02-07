e3593fd215fed384bdd7332863822d5955893b4d
Administrator <182316962@qq.com>
2018-02-07 23:45:00 +0800

    http config change

--- src/http/config.js
+++ src/http/config.js
@@ -1,7 +1,8 @@
 
 const config = {
-    baseURL:'http://47.95.219.45:3000/'
+    // baseURL:'http://47.95.219.45:3000/'
     // baseURL:'http://localhost:3000/'
+    baseURL:'http://123.207.59.80:3000/'
     
 }
 
